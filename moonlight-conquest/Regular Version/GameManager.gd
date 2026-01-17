595a8481db055b3e64835ccd1c377820d55ef1e9
AeroBotX <p.garg1606@gmail.com>
2026-01-17 17:52:37 -0500

    Created player 1 and player 1 name plus realm pick scenes. Player 3-6 left

--- moonlight-conquest/Regular Version/GameManager.gd
+++ moonlight-conquest/Regular Version/GameManager.gd
@@ -1,29 +1,21 @@
-extends Node2D  
+extends Node
 
 var player_data = [] 
 var current_player_index = 0
+var max_players = 6
 
-# Configuration
-var max_players = 6 
-var picks_per_realm = 1 
+# List your scenes here in the exact order you want them to appear
+var selection_scenes = [
+	"res://playerone.tscn",
+	"res://playertwi.tscn",
+	"res://playerthree.tscn",
+	"res://playerfour.tscn",
+	"res://playerfive.tscn",
+	"res://playersix.tscn"
+]
 
-var realm_counts = {
-	"air": 0, "dream": 0, "earth": 0, 
-	"fire": 0, "water": 0, "time": 0
-}
-
-func is_realm_available(realm_name: String) -> bool:
-	return realm_counts.get(realm_name, 0) < picks_per_realm
+var realm_counts = {"air": 0, "dream": 0, "earth": 0, "fire": 0, "water": 0, "time": 0}
 
 func save_player(player_name: String, realm_name: String):
-	player_data.append({
-		"name": player_name,
-		"realm": realm_name
-	})
+	player_data.append({"name": player_name, "realm": realm_name})
 	realm_counts[realm_name] += 1
-
-func reset_game_data():
-	player_data.clear()
-	current_player_index = 0
-	for key in realm_counts.keys():
-		realm_counts[key] = 0

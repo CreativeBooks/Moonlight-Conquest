308f4604e6d77f730a9241d5308fdb1901a5ba54
AeroBotX <p.garg1606@gmail.com>
2026-01-24 17:51:54 -0500

    edited rules page

--- moonlight-conquest/Menu Scenes + Script/rules.gd
+++ moonlight-conquest/Menu Scenes + Script/rules.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _on_returnbutton_pressed() -> void:
 	get_tree().change_scene_to_file("res://Menu Scenes + Script/node_2d.tscn")
+
+
+func _on_starbutton_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menu Scenes + Script/node_2d.tscn")
